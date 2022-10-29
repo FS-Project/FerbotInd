32815d3b5dcc29165ae5c599846e6e3010ba82cd
fsproject <fernans1.bot@gmail.com>
2022-10-29 17:45:01 +0700

    Make a Changes

--- ferbot/modules/helper_funcs/fun_strings.py
+++ ferbot/modules/helper_funcs/fun_strings.py
@@ -421,37 +421,29 @@ DECIDE = ("Yes.", "NoU.", "Maybe.", "No.")
 
 # AFK strings credits: PaperPlaneExtended!
 AFKRPL = (
-    "<b>{}</b> is busy right now. Please talk in a bag and when they come back you can just give him the bag!",
-    "<b>{}</b> is away right now. If you need anything, leave a message after the beep:\n<code>beeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeep</code>!",
-    "You just missed <b>{}</b>, next time aim better.",
-    "{} will be back in a few minutes and if not...,\nwait longer.",
-    "Roses are red,\nViolets are blue,\nLeave me a message,\nAnd {} will get back to you.",
-    "If you haven't figured it out already,\n{} is not here.",
-    "{} went that way\n---->",
-    "{} went went this way\n<----",
-    "{} is not here so stop writing to them,\nor else you will find yourself with a screen full of your own messages.",
-    "Life is so short, there are so many things to do...\n{} is away doing one of them..",
-    "{} is away from the keyboard at the moment, but if you'll scream loud enough at your screen, they might just hear you.",
+    "<b>{}</b> sedang istirahat sekarang",
+    "<b>{}</b> jika kamu ada perlu silahkan tinggalkan pesan!",
+    "{} jangan diganggu! Dia sedang istirahat",
+    "Kamu sedang berbicara dengan {}, dia tidak tersedia sekarang!",
+    "{} sedang offline sekarang!",
 )
 
 NOAFK = (
-    "{} is here!",
-    "{} is back!",
-    "{} is now in the chat!",
-    "{} is awake!",
-    "{} is back online!",
-    "{} is finally here!",
-    "Welcome back! {}",
-    "OwO {} is here!",
+    "{} Telah kembali!",
+    "{} Sekarang sedang Aktif disini!",
+    "{} Sudah bangun dari tidur!",
+    "{} Sudah Aktif kembali!",
+    "{} Akhirnya kembali!",
+    "Selamat datang kembali! {}",
 )
 
 AFK = (
-    "Cya 👋 {}!",
+    "Selamat tinggal {}!",
     "Byebye {}.",
     "See you later {}!",
     "Goodbye {}!",
     "Come back soon {}!",
-    "Kthnxbye {}..",
+    "Kembali lagi yaa {}..",
 )
 
 SHGS = (
