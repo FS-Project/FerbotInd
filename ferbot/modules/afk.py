7c87a460f11cea6a4907684b3d4eb9ceb3822199
fsproject <fernans1.bot@gmail.com>
2022-10-29 17:31:47 +0700

    Make a Changes

--- ferbot/modules/afk.py
+++ ferbot/modules/afk.py
@@ -19,7 +19,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """Afk module: Tell anyone if you away from keyboard."""
 import random
-from time import sleep
 
 from telegram import MessageEntity
 from telegram.error import BadRequest
@@ -65,7 +64,6 @@ def afk(update, context):
     afksend = msg.reply_text(
         afkstr.format(update.effective_user.first_name, notice)
     )
-    sleep(5)
     try:
         afksend.delete()
     except BadRequest:
@@ -97,7 +95,6 @@ def no_longer_afk(update, context):
             unafk = update.effective_message.reply_text(
                 chosen_option.format(firstname)
             )
-            sleep(10)
             unafk.delete()
         except BaseException:
             return
@@ -177,7 +174,6 @@ def check_afk(update, context, user_id, fst_name, userc_id):
             replafk = update.effective_message.reply_text(
                 res, parse_mode="html"
             )
-        sleep(10)
         try:
             replafk.delete()
         except BadRequest:
